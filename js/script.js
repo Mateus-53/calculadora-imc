06f1bbd2b68ac1b2a313aa829718971e76e0f3f0
Mateus Ferreira <121569308+Mateus-53@users.noreply.github.com>
2023-04-07 00:10:58 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,55 @@
 let calcBtn = document.querySelector("button");
+const colors = {
+  font: "#e7f6f2",
+  bg: "#395b64",
+  chooseBg: "#a5c9ca",
+  chooseFont: "#112031",
+};
 calcBtn.onclick = function calcImc() {
   let pesoValue = document.querySelector("#peso").value,
     alturaValue = document.querySelector("#altura").value,
-    imcValue = document.querySelector(".imcValue");
-  formImc = pesoValue / alturaValue ** 2;
+    imcValue = document.querySelector(".imcValue"),
+    calcImc = pesoValue / alturaValue ** 2;
 
   if (alturaValue == "" && pesoValue == "") {
-    alert("É necessário informar seu peso e sua altura para calcular seu IMC!");
+    alert("É necessário informar: \n > Peso \n > Altura");
   } else if (alturaValue == "") {
-    alert("É necessário informar sua altura para calcular o seu IMC!");
+    alert("É necessário informar: \n > Altura");
   } else if (pesoValue == "") {
-    alert("É necessário informar seu peso para calcular o seu IMC!");
+    alert("É necessário informar: \n > Peso");
   } else {
-    imcValue.innerHTML = `${formImc.toFixed(2)}`;
+    imcValue.innerHTML = `${calcImc.toFixed(2)}`;
+    const tr = document.querySelectorAll(".trClass");
+    for (let i = 0; i < tr.length; i++) {
+      let chooseTr = {
+        tr1: tr[0],
+        tr2: tr[1],
+        tr3: tr[2],
+        tr4: tr[3],
+        tr5: tr[4],
+        tr6: tr[5],
+      };
+
+      if (calcImc <= 18.5) {
+        chooseTr.tr1.style.backgroundColor = colors.chooseBg;
+        chooseTr.tr1.style.color = colors.chooseFont;
+      } else if (calcImc >= 18.6 && calcImc <= 24.9) {
+        chooseTr.tr2.style.backgroundColor = colors.chooseBg;
+        chooseTr.tr2.style.color = colors.chooseFont;
+      } else if (calcImc >= 25 && calcImc <= 29.9) {
+        chooseTr.tr3.style.backgroundColor = colors.chooseBg;
+        chooseTr.tr3.style.color = colors.chooseFont;
+      } else if (calcImc >= 30 && calcImc <= 34.9) {
+        chooseTr.tr4.style.backgroundColor = colors.chooseBg;
+        chooseTr.tr4.style.color = colors.chooseFont;
+      } else if (calcImc >= 35 && calcImc <= 39.9) {
+        chooseTr.tr5.style.backgroundColor = colors.chooseBg;
+        chooseTr.tr5.style.color = colors.chooseFont;
+      } else if (calcImc >= 40) {
+        chooseTr.tr6.style.backgroundColor = colors.chooseBg;
+        chooseTr.tr6.style.color = colors.chooseFont;
+      }
+    }
   }
 };
 
@@ -22,6 +59,12 @@ resetBtn.onclick = function resetValue() {
     alturaValue = document.querySelector("#altura").value,
     imcValue = document.querySelector(".imcValue");
 
+  const tr = document.querySelectorAll(".trClass");
+  for (let i = 0; i < tr.length; i++) {
+    let chooseTr = tr[i];
+    chooseTr.style.backgroundColor = colors.bg;
+    chooseTr.style.color = colors.font;
+  }
   pesoValue = document.querySelector("#peso").value = "";
   alturaValue = document.querySelector("#altura").value = "";
   imcValue.innerHTML = "";
